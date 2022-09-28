d35e340d38b923b98fa29984506dc671a33b20ad
Mayur-Pc <sagar12500@gmail.com>
2022-09-28 21:59:56 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@
 import './App.css';
 import { Allroute } from './components/Allroute';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
 function App() {
   return (
     <div className="App">
