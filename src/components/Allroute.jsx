f05889e803cfa79cd7b5701a374c607364542164
Akshaya Annadurai <akshaysrij@gmail.com>
2022-09-29 21:24:39 +0530

    customers

--- src/components/Allroute.jsx
+++ src/components/Allroute.jsx
@@ -3,14 +3,16 @@ import Home from "../Pages/Home"
 import Integration from "../Pages/Integration"
 import LoginSignup from "../Pages/LoginSignup"
 import WhyHarvest from "../Pages/WhyHarvest"
+import Customers from "./Customers/Customers"
+import Features from "./Features/Features"
 
 export const Allroute =()=>{
 return<div>
 <Routes>
 <Route path="/" element={<Home/>}/>
 <Route path="/Why-Harvest" element={<WhyHarvest />}/>
-<Route path="/Features" element={<h1>Features</h1>}/>
-<Route path="/Customers" element={<h1>Customers</h1>}/>
+<Route path="/Features" element={<Features />}/>
+<Route path="/Customers" element={<Customers />}/>
 <Route path="/Integrations" element={<Integration />}/>
 <Route path="/Pricing" element={<h1>Pricing</h1>}/>
 <Route path="/signin" element={<LoginSignup/>}/>
