90210ea16d21681f80964395338e07e7e8e1665f
Sathankothandam <sathankothandam@gmail.com>
2022-09-29 22:44:06 +0530

    updating price

--- src/components/Allroute.jsx
+++ src/components/Allroute.jsx
@@ -2,6 +2,7 @@ import {Routes,Route} from "react-router-dom"
 import Home from "../Pages/Home"
 import Integration from "../Pages/Integration"
 import LoginSignup from "../Pages/LoginSignup"
+import Pricing from "../Pages/Pricing"
 export const Allroute =()=>{
 return<div>
 <Routes>
@@ -10,7 +11,7 @@ return<div>
 <Route path="/Features" element={<h1>Features</h1>}/>
 <Route path="/Customers" element={<h1>Customers</h1>}/>
 <Route path="/Integrations" element={<Integration />}/>
-<Route path="/Pricing" element={<h1>Pricing</h1>}/>
+<Route path="/Pricing" element={<Pricing/>}/>
 <Route path="/signin" element={<LoginSignup/>}/>
 <Route path="/signup" element={<h1>Sign Up</h1>}/>
 
