b54c71e799e24222641dbaf6d8bcca4d0f533d0c
Kiran Kamble <kirankamble4595@gmail.com>
2022-09-30 23:33:50 +0530

    everything is working so far

--- src/components/Customers/Customers.jsx
+++ src/components/Customers/Customers.jsx
@@ -1,174 +1,159 @@
-
-import React from 'react'
-import styled from 'styled-components'
-import {mobile } from "../integration/IntegrationResponsive"
-import TodayTime from '../integration/TodayTime'
-import CustomerContainer2 from './CustomerContainer2'
-import Customerlogos from './Customerlogos'
-import SearchCustomer from './SearchCustomer'
+import React from "react";
+import styled from "styled-components";
+import { mobile } from "../integration/IntegrationResponsive";
+import TodayTime from "../integration/TodayTime";
+import CustomerContainer2 from "./CustomerContainer2";
+import Customerlogos from "./Customerlogos";
+import SearchCustomer from "./SearchCustomer";
 
 const Customers = () => {
-    const data = [
-        {
-            id: 0,
-            img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/icons/illo-70-person.svg",
-            count: "70,000+",
-            name: "customers",
-        },
-        {
-            id: 1,
-            img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/icons/illo-70-keep.svg",
-            count: "2 billion",
-            name: "hours tracked",
-        },
+  const data = [
+    {
+      id: 0,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/icons/illo-70-person.svg",
+      count: "70,000+",
+      name: "customers",
+    },
+    {
+      id: 1,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/icons/illo-70-keep.svg",
+      count: "2 billion",
+      name: "hours tracked",
+    },
 
-        {
-            id: 2,
-            img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/icons/illo-70-money.svg",
-            count: "19.6 million",
-            name: "invoices paid",
-        },
+    {
+      id: 2,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/icons/illo-70-money.svg",
+      count: "19.6 million",
+      name: "invoices paid",
+    },
+  ];
+  const imgData = [
+    {
+      id: 0,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-vw.svg?noresize",
+    },
+    {
+      id: 1,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-aclu.svg?noresize",
+    },
 
+    {
+      id: 2,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-conde.svg?noresize",
+    },
+    {
+      id: 3,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-dell.svg?noresize",
+    },
+    {
+      id: 4,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-amnesty.svg?noresize",
+    },
 
-    ]
-    const imgData = [
-        {
-            id: 0,
-            img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-vw.svg?noresize",
+    {
+      id: 5,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-deloitte.svg?noresize",
+    },
+    {
+      id: 6,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-yale.svg?noresize",
+    },
+  ];
 
-        },
-        {
-            id: 1,
-            img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-aclu.svg?noresize",
-
-        },
-
-        {
-            id: 2,
-            img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-conde.svg?noresize",
-
-        },
-        {
-            id: 3,
-            img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-dell.svg?noresize",
-
-        },
-        {
-            id: 4,
-            img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-amnesty.svg?noresize",
-
-        },
-
-        {
-            id: 5,
-            img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-deloitte.svg?noresize",
-
-        },
-        {
-            id: 6,
-            img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-yale.svg?noresize",
-
-        },
-    ]
-
-    const Container = styled.div`
-    width:100%;
-  margin:auto;
-    `
-    const Heading=styled.h1`
+  const Container = styled.div`
+    width: 100%;
+    margin: auto;
+  `;
+  const Heading = styled.h1`
     font-size: 50px;
     line-height: 60px;
     font-weight: 400;
     margin-bottom: 21px;
-    margin-top:60px;
-    `
-    const SmallHeading=styled.h1`
-    
+    margin-top: 60px;
+  `;
+  const SmallHeading = styled.h1`
     font-size: 29px;
     line-height: 37.7px;
-    
-    `
-    const Cart = styled.div`
+  `;
+  const Cart = styled.div`
     display: flex;
-    margin-top:50px;
-    margin:auto;
-padding-bottom:5%;
-padding-top:5%;
-  margin-left:10%;
-   
-   ${mobile({flexDirection:"column",width:"auto"})}
-    `
-    const Box = styled.div`
-    width:30%;
-    border:1px solid #fa5d00;
-    height:auto;
+    margin-top: 50px;
+    margin: auto;
+    padding-bottom: 5%;
+    padding-top: 5%;
+    margin-left: 10%;
+
+    ${mobile({ flexDirection: "column", width: "auto" })}
+  `;
+  const Box = styled.div`
+    width: 30%;
+    border: 1px solid #fa5d00;
+    height: auto;
     display: flex;
-    justify-content:space-between;
-    align-items:center;
-    padding:20px;
-    border-left:none;
-    
-    ${mobile({width:"100%",margin:"auto"})}
-   `
-    const LittleBox1 = styled.div`
-    width:70%;
-    text-align:start;
-    align-items:flex-start;
-   
-   `
-    /* const LittleBox2 = styled.div`
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    border-left: none;
+
+    ${mobile({ width: "100%", margin: "auto" })}
+  `;
+  const LittleBox1 = styled.div`
+    width: 70%;
+    text-align: start;
+    align-items: flex-start;
+  `;
+  /* const LittleBox2 = styled.div`
     width:30%;
    ` */
-    const Count=styled.h4`
-   
+  const Count = styled.h4`
     font-size: 42px;
     line-height: 42px;
-    `
-    const Name=styled.h5`
-   
+  `;
+  const Name = styled.h5`
     font-size: 26px;
     line-height: 26px;
-    `
-    const Image=styled.img`
-        width:30%;
-    `
-    const Line = styled.hr`
+  `;
+  const Image = styled.img`
+    width: 30%;
+  `;
+  const Line = styled.hr`
     margin-top: 3%;
     width: 100%;
     height: 1px;
     background-color: red;
     color: red;
-    ${mobile({width:"100%" })}
-  
+    ${mobile({ width: "100%" })}
   `;
-    return (
-        <>
-        <Container>
-            <Heading>
-                See how Harvest helps teams like yours thrive
-            </Heading>
-            <SmallHeading>Teams of all sizes, across industries, and around the world track time with Harvest.</SmallHeading>
-            <Cart>
-            {data.map((el)=>(<Box key={el.id}>
-                <LittleBox1>
+  return (
+    <>
+      <Container>
+        <Heading>See how Harvest helps teams like yours thrive</Heading>
+        <SmallHeading>
+          Teams of all sizes, across industries, and around the world track time
+          with Harvest.
+        </SmallHeading>
+        <Cart>
+          {data.map((el) => (
+            <Box key={el.id}>
+              <LittleBox1>
                 <Count>{el.count}</Count>
                 <Name>{el.name}</Name>
-                </LittleBox1>
-                
-                <Image src={el.img} />
-               
-                </Box>))}
-            </Cart>
-        </Container>
-        <CustomerContainer2 />
-        <Customerlogos />
-        <Line />
-        <SearchCustomer />
-        <Line />
-        <TodayTime />
-        </>
+              </LittleBox1>
 
-    )
-}
+              <Image src={el.img} />
+            </Box>
+          ))}
+        </Cart>
+      </Container>
+      <CustomerContainer2 />
+      <Customerlogos />
+      <Line />
+      <SearchCustomer />
+      <Line />
+      <TodayTime />
+    </>
+  );
+};
 
-export default Customers
+export default Customers;
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,154 +1,140 @@
-import React from 'react'
+import React from "react";
 // import { Box, Heading, HStack, Stack, Image, Divider, Button, Icon } from '@chakra-ui/react'
 // import style from "./feature.module.css"
- import Timetracking from './Timetracking'
+import Timetracking from "./Timetracking";
 // import Reports from './Reports'
 // import Invoicing from './Invoicing'
 
-import styled from 'styled-components'
-import {mobile } from "../integration/IntegrationResponsive"
-import Reports from './Reports'
-import Invoicing from './Invoicing'
-import TodayTime from '../integration/TodayTime'
+import styled from "styled-components";
+import { mobile } from "../integration/IntegrationResponsive";
+import Reports from "./Reports";
+import Invoicing from "./Invoicing";
+import TodayTime from "../integration/TodayTime";
 
 const Features = () => {
-    const data=[
-       { id:0,
-        img:"https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/features/illo-nav-time.svg",
-        name:"Time tracking",
-       },
-       { id:1,
-        img:"https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/features/illo-nav-report.svg",
-        name:"Reports & Analysis",
-       },
-       { id:2,
-        img:"https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/features/illo-nav-invoice.svg",
-        name:"Invoicing & payments",
-       },
-       
+  const data = [
+    {
+      id: 0,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/features/illo-nav-time.svg",
+      name: "Time tracking",
+    },
+    {
+      id: 1,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/features/illo-nav-report.svg",
+      name: "Reports & Analysis",
+    },
+    {
+      id: 2,
+      img: "https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/features/illo-nav-invoice.svg",
+      name: "Invoicing & payments",
+    },
+  ];
 
-    ]
-
-    const Container = styled.div`
-    width:80%;
-  margin:auto;
-  align-items:center;
-  text-align:start;
-    `
-    const Heading=styled.h1`
+  const Container = styled.div`
+    width: 80%;
+    margin: auto;
+    align-items: center;
+    text-align: start;
+  `;
+  const Heading = styled.h1`
     font-size: 50px;
     line-height: 60px;
     font-weight: 400;
     margin-bottom: 21px;
-    margin-top:60px;
-   
-    `
-    const SmallHeading=styled.h1`
-    
+    margin-top: 60px;
+  `;
+  const SmallHeading = styled.h1`
     font-size: 29px;
     line-height: 37.7px;
-   
-    
-    `
-    const Cart = styled.div`
+  `;
+  const Cart = styled.div`
     display: flex;
-    margin-top:50px;
-    margin:auto;
-padding-bottom:5%;
-padding-top:5%;
- 
-   gap:10px;
-   ${mobile({flexDirection:"column",width:"auto"})}
-    `
-    const Box = styled.div`
-    width:30%;
-    border:1px solid #fa5d00;
-    border-radius:16px;
-    height:auto;
+    margin-top: 50px;
+    margin: auto;
+    padding-bottom: 5%;
+    padding-top: 5%;
+
+    gap: 10px;
+    ${mobile({ flexDirection: "column", width: "auto" })}
+  `;
+  const Box = styled.div`
+    width: 30%;
+    border: 1px solid #fa5d00;
+    border-radius: 16px;
+    height: auto;
     display: flex;
-    justify-content:space-between;
-   align-items:center;
-    padding:20px;
-   
-    
-    ${mobile({width:"100%",margin:"auto"})}
-   `
-    const LittleBox1 = styled.div`
-    width:70%;
-   text-align:start;
-    justify-content:space-between;
-   display: flex;
-   `
-    /* const LittleBox2 = styled.div`
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+
+    ${mobile({ width: "100%", margin: "auto" })}
+  `;
+  const LittleBox1 = styled.div`
+    width: 70%;
+    text-align: start;
+    justify-content: space-between;
+    display: flex;
+  `;
+  /* const LittleBox2 = styled.div`
     width:30%;
    ` */
-    // const Count=styled.h4`
-   
-    // font-size: 42px;
-    // line-height: 42px;
-    // `
-    const Name=styled.h5`
-   
+  // const Count=styled.h4`
+
+  // font-size: 42px;
+  // line-height: 42px;
+  // `
+  const Name = styled.h5`
     font-size: 24px;
     line-height: 31.2px;
-    `
-    const Image=styled.img`
-        width:20%;
-    `
-    const Line = styled.hr`
+  `;
+  const Image = styled.img`
+    width: 20%;
+  `;
+  const Line = styled.hr`
     margin-top: 3%;
     width: 100%;
     height: 1px;
     background-color: #fa5d00;
     color: #fa5d00;
-    ${mobile({width:"100%" })}
-  
+    ${mobile({ width: "100%" })}
   `;
-  const Icon=styled.img`
-  width:24px;
-`
-const FeatureContainer=styled.div`
-margin:auto;
-align-items:center;`
+  const Icon = styled.img`
+    width: 24px;
+  `;
+  const FeatureContainer = styled.div`
+    margin: auto;
+    align-items: center;
+  `;
   return (
     <>
-   
-    <Container>
-    <Heading>
-    Everything your team needs to keep ticking
-    </Heading>
-    <SmallHeading>Intuitive time tracking and powerful insights that help your team thrive.</SmallHeading>
-    <Cart>
-    {data.map((el)=>(<Box key={el.id}>
-      <Image src={el.img} />
-        <LittleBox1>
-      
-        <Name>{el.name}</Name>
-        <Icon src="https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/icons/arrow-down-24.svg" />
-        </LittleBox1>
-        
-        
-       
-        </Box>))}
-    </Cart>
-</Container>
-<Line />
-<Timetracking />
-<Line />
-<Reports />
-<Line />
-<Invoicing />
-<Line />
-<TodayTime />
+      <Container>
+        <Heading>Everything your team needs to keep ticking</Heading>
+        <SmallHeading>
+          Intuitive time tracking and powerful insights that help your team
+          thrive.
+        </SmallHeading>
+        <Cart>
+          {data.map((el) => (
+            <Box key={el.id}>
+              <Image src={el.img} />
+              <LittleBox1>
+                <Name>{el.name}</Name>
+                <Icon src="https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/icons/arrow-down-24.svg" />
+              </LittleBox1>
+            </Box>
+          ))}
+        </Cart>
+      </Container>
+      <Line />
+      <Timetracking />
+      <Line />
+      <Reports />
+      <Line />
+      <Invoicing />
+      <Line />
+      <TodayTime />
+    </>
+  );
+};
 
-</>
-
-  
-
-  
-  
-   
-  )
-}
-
-export default Features
+export default Features;
